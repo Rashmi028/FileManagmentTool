581173ed985a300039a0b092353a7eea6413bcfc
Rashmi028 <vermarashmi028@gmail.com>
2024-06-14 13:26:02 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from "express"
+import path from "path"
+import fs from "fs"
+import http from "http"
+
+const app= express();
+
+const server =http.createServer(function(req,res){
+   res.write("HEllo world");
+   fs.writeFile
+   res.end();
+})
+
+server.listen(8000, () => {
+    console.log(`Server running on port 8000`);
+  });
+  
